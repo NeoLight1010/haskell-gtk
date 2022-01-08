f1527aeffbeea2d8ab9db25fafedb243960f1e59
Anthony Suárez <neolight1010@gmail.com>
2022-01-08 11:54:59 -0500

    Add simple Click Me! Button.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,47 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Control.Monad
+import GI.Gtk.Declarative
+import GI.Gtk.Declarative.App.Simple
+import GI.Gtk.Objects hiding (Widget)
+
+data State = Clicked | NotClicked
+
+data Event = ButtonClicked | Quit
+
+update' :: State -> Event -> Transition State Event
+update' _ Quit = Exit
+update' _ ButtonClicked = Transition Clicked (return Nothing)
+
+drawWindow w s =
+  bin
+    Window
+    [ #title := "Hello World!",
+      on #deleteEvent $ const (True, Quit)
+    ]
+    $ w s
+
+drawBody :: State -> Widget Event
+drawBody Clicked = widget Button [#label := "Thank you!"]
+drawBody NotClicked =
+  widget
+    Button
+    [ #label := "Click me!",
+      on #clicked ButtonClicked
+    ]
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main =
+  void $
+    run
+      App
+        { view = drawWindow drawBody,
+          update = update',
+          inputs = [],
+          initialState = NotClicked
+        }
